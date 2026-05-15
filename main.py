4f60cdf201eeacee4e4b3694bcdaf31ae44010a6
kbinwal7 <khusheebinwal29@gmail.com>
2026-05-15 08:44:08 +0530

    set python version

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://expense-tracker-1-1tjd.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
